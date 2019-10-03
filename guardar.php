173b8a246501384926627be4364bb9a3b638886f
Gaston Arevalo <arevalogaston99@gmail.com>
2019-10-03 12:04:32 -0300

    Agrego metodo para tomar datos del formulario por GET y/o POST

---
+++ guardar.php
@@ -0,0 +1,13 @@
+<?php
+
+$HOLA = 5;
+echo $HOLA;
+
+/*echo $_GET['titulo'];
+echo $_GET['descrip'];
+
+if(isset($_GET['titulo']) $$ isset($_GET['descrip'])){
+
+    echo $_GET['titulo'];
+    echo $_GET['descrip'];
+}*/
